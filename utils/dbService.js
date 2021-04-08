3aa0bef14a25bb5c3b23fd291bb9fd12a2fde61e
unknown <ondrej.richnak@gmail.com>
2021-04-08 11:58:33 +0200

    p83

--- utils/dbService.js
+++ utils/dbService.js
@@ -25,10 +25,11 @@ function handleDisconnect() {
     });                                     // process asynchronous requests in the meantime.
                                             // If you're also serving http, display a 503 error.
     connection.on('error', function(err) {
-        console.log('db error', err);
+        //console.log('db error', err);
         if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
             handleDisconnect();                         // lost due to either server restart, or a
         } else {                                      // connnection idle timeout (the wait_timeout
+            console.log('db error', err);
             throw err;                                  // server variable configures this)
         }
     });
